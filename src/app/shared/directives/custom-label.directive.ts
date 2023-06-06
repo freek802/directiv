4e152c95f8105135f2046108c9290d1560502edd
Omar.Sanchez <freek802@gmail.com>
2023-06-06 08:42:12 -0600

    change

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,63 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
+
+@Directive({
+  selector: '[customLabel]',
+})
+export class CustomLabelDirective implements OnInit {
+  private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
+
+  @Input()
+  set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
+
+  @Input()
+  set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
+  }
+
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el;
+  }
+
+  ngOnInit(): void {}
+
+  setStyle(): void {
+    if (!this.htmlElement) return;
+
+    this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.hidden = true;
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'This field is required';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+      this.htmlElement.nativeElement.innerText = `This field needs to have min ${current}/${min}  characters`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText =
+        'Incorrect format for an email';
+      return;
+    }
+  }
+}
